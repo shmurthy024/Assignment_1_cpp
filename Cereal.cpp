6991ddf799ad7920f16e347588a2e682c5eceb94
shmurthy024 <shreemurthy04@gmail.com>
2021-01-25 02:39:56 +0000

    final

---
+++ Cereal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+//metric ton is 35,273.92 so use int
+int main(){
+
+  double metricTon = 35273.92;
+  double packageOfCerealInOz;
+
+  cout << "Enter the package of breakfast cereal in ounces: ";
+  cin >> packageOfCerealInOz;
+
+  cout << packageOfCerealInOz / metricTon << " metric tons" << endl;
+
+
+  return 0;
+
+}
